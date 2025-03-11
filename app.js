7107e071ba10ed46852f29ae2d8adb79aa7d9fd1
Lucas Contreras <lucasblas1234csj@gmail.com>
2025-03-10 19:40:20 -0500

    Keep learning

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-// Let Keyword
-let USD = 1200;
-let AUD = 1.5;
-AUD = USD * AUD
-console.log(AUD)
-// Const keyword
 
+// for(let i=1; i <= 15; i++){
+//   if(i % 5 == 0){
+//        console.log( i + " - ASAP FRONTEND")
+//    }
+//    else if (i % 2 == 0){
+//        console.log(i + " - Frontend")
+//    }
+//    else{
+//        console.log(i + " - ASAP")
+//    }
+//}
 
+let str = "ASAP FRONTEND";
+for (let i = 0; i < str.length; i++){
+    console.log(str[i])
+}
