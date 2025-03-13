777de54a0bc025b349e39fd69780ad17b5332874
Lucas Contreras <lucasblas1234csj@gmail.com>
2025-03-13 11:06:57 -0500

    Updated index,style and adding other pages

--- app.js
+++ app.js
@@ -11,7 +11,26 @@
 //    }
 //}
 
-let str = "ASAP FRONTEND";
-for (let i = 0; i <= str.length; i++){
-    console.log(str[i])
-}
+
+//        let str = "ASAP FRONTEND";
+//       for (let i = 0; i <= str.length; i++){
+//            console.log(str[i])
+//        }
+
+// Function Declaration 
+//function greetUser(name, time){
+//    console.log(`Good ${time}, ${name}`);
+//}
+
+//greetUser("Iman","morning");
+//greetUser("Alex","afternoon");
+//greetUser("Jhon","night");
+
+// Create a fucntion that converts $USD into $AUD
+/**
+ * $AUD to $USD Formula:
+ * AUD = USD * 1.5
+ */
+
+let array = ["Ganyu","Keqing","Jane Doe","Mavuika","Lisa"]
+console.log(array[array.length-1])
